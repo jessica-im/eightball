429417339aa4311e78ee8f97c3d9dbda899eab51
Jessica Im <me@jessicaim.com>
2021-12-06 14:05:11 -0500

    updated places api urls.py

--- places_api/urls.py
+++ places_api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('places', views.PlaceList.as_view(), name='place_list'),
-    path('places/<int:pk>', views.PlaceDetail.as_view(), name='place_detail'),
+    path('api/places', views.PlaceList.as_view(), name='place_list'),
+    path('api/places/<int:pk>', views.PlaceDetail.as_view(), name='place_detail'),
 ]
